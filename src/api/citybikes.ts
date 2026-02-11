23e7eff76e66e04e710d2052042393738011de53
Kevin <kevin@example.com>
2026-02-11 14:55:31 -0300

    fix: use https for citybikes api to prevent mixed content errors

--- src/api/citybikes.ts
+++ src/api/citybikes.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Network, NetworkDetail } from '../types';
 
-const API_URL = 'http://api.citybik.es/v2/networks';
+const API_URL = 'https://api.citybik.es/v2/networks';
 
 export const fetchNetworks = async (): Promise<Network[]> => {
     const response = await axios.get<{ networks: Network[] }>(API_URL);
